ccfc21efa8f73036b9eb05d7a63c6e89cd5f30ae
lpereyrasantiago <34422960+lpereyrasantiago@users.noreply.github.com>
2018-04-10 14:55:13 -0300

    Update Daycycle.cs

--- Scripts/Daycycle.cs
+++ Scripts/Daycycle.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Daycycle : MonoBehaviour {
 
+	// Mostly all of this script is only necessary if you want to tweak your light and your fog to make them
+	// different through night and day (and that's what makes sense).  If you do not want that,
+	// just use variable of line 11, line 28 of the Start method and all Update method but ManageDayNightColors().
+	
 	private Quaternion startRotation;
 	private Light sunLight;
-	private Color sunDayLightColor, fogDayColor;
+	private Color sunDayLightColor, fogDayColor; //
 	private float lightDayIntensity;
 	private bool dayArrived = false, nightArrived = false;
 	public TimeManager timeManager;
@@ -26,7 +30,6 @@ public class Daycycle : MonoBehaviour {
 
 	void Update () {
 		ManageDayNightColors ();
-		//float angleThisFrame = Time.deltaTime / 360 * timeManager.minutesPerSecond;
 		float angleThisFrame = Time.deltaTime * timeManager.sunVelocity;
 		transform.RotateAround (transform.position, Vector3.right, angleThisFrame);
 	}
@@ -34,6 +37,9 @@ public class Daycycle : MonoBehaviour {
 	void ManageDayNightColors () {
 		if (timeManager.GetCurrentTime () > 0.0f && timeManager.GetCurrentTime () < 0.5) {
 			if (dayArrived == false) {
+				// HERE TWEAK YOUR LIGHTS AND FOG IF NEEDED.
+				// THE NEXT, EXCEPT FOR THE BOOLEANS, ARE ONLY EXAMPLES: CHANGE THEM TO SUIT YOUR NEEDS.
+				
 				sunLight.color = sunDayLightColor;
 				RenderSettings.fogColor = fogDayColor;
 				sunLight.intensity = lightDayIntensity;
